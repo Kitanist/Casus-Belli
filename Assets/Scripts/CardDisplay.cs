254c1db57940ada658f16d6c6d8a089cec014338
Kitanist <eneskerem2516@gmail.com>
2023-02-20 00:00:22 +0300

    CardDisplayUpdate

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -12,17 +12,22 @@ public class CardDisplay : MonoBehaviour
     public TMP_Text descriptionText, descriptionText2;
     
     public Image artworkImage,ClassImage,skill1Image,skill2Image;
+    public GameObject ArtworkMat, ClassMat, skill1Mat, skill2Mat;
 
     private void Start()
     {
+        
         nameText.text = card.name;
         descriptionText.text = card.description1;
         descriptionText2.text = card.description2;
         Debug.Log(card.cardID);
-        artworkImage.sprite = card.artwork;
-        ClassImage.sprite = card.Class;
-        skill1Image.sprite = card.skill1;
-        skill2Image.sprite = card.skill2;
-
+        //   artworkImage.sprite = card.artwork;
+        //   ClassImage.sprite = card.Class;
+        //   skill1Image.sprite = card.skill1;
+        //   skill2Image.sprite = card.skill2;
+        ArtworkMat.GetComponent<Renderer>().material = card.artworkMat;
+        ClassMat.GetComponent<Renderer>().material = card.ClassMat;
+        skill1Mat.GetComponent<Renderer>().material = card.Skill1Mat;
+        skill2Mat.GetComponent<Renderer>().material = card.Skill2Mat;
     }
 }
