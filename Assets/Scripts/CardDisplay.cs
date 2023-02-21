ae262119a979e011ace2532922f629e3c0b5b185
Kitanist <eneskerem2516@gmail.com>
2023-02-21 15:56:46 +0300

    Card Prefab Update and Card UI update
    bir gün bir enes varmış ölmüş mutlu son

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -10,27 +10,46 @@ public class CardDisplay : MonoBehaviour
 
     public TMP_Text nameText;
     public TMP_Text descriptionText, descriptionText2;
-    
-    //public Image artworkImage,ClassImage,skill1Image,skill2Image;
-    public GameObject ArtworkMat, ClassMat, skill1Mat, skill2Mat;
+    public bool ui;
+    public Image artworkImage,ClassImage,skill1Image,skill2Image;
+    public GameObject ArtworkMat, ClassMat, skill1Mat, skill2Mat,speedMat,KartUI;
 
     private void Start()
     {
-        
-   
+      
     }
     public void Init () {
         nameText.text = card.name;
         descriptionText.text = card.description1;
         descriptionText2.text = card.description2;
-        
-        //   artworkImage.sprite = card.artwork;
-        //   ClassImage.sprite = card.Class;
-        //   skill1Image.sprite = card.skill1;
-        //   skill2Image.sprite = card.skill2;
+       
+         
         ArtworkMat.GetComponent<Renderer>().material = card.artworkMat;
         ClassMat.GetComponent<Renderer>().material = card.ClassMat;
         skill1Mat.GetComponent<Renderer>().material = card.Skill1Mat;
         skill2Mat.GetComponent<Renderer>().material = card.Skill2Mat;
+        speedMat.GetComponent<Renderer>().material = card.Speedmat;
+    }
+    public void InitUI()
+    {
+        if (ui)
+        {
+            KartUI.SetActive(true);
+            ui = false;
+        }
+        else if (!ui)
+        {
+            KartUI.SetActive(false);
+            ui = true;
+        }
+        
+            artworkImage.sprite = card.artwork;
+            ClassImage.sprite = card.Class;
+            skill1Image.sprite = card.skill1;
+            skill2Image.sprite = card.skill2;
+            nameText.text = card.name;
+            descriptionText.text = card.description1;
+            descriptionText2.text = card.description2;
+
     }
 }
