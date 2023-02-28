ad8fa409ec8f85dfcb7f769cb454980cb6433ae0
ServerYıldız <77243657+ServerYLDZ@users.noreply.github.com>
2023-02-28 16:00:01 +0300

    battleManager-3

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -26,11 +26,12 @@ public class BattleManager :MonoSingeleton<BattleManager>
 
     public bool prensIsBlockedPlayer=false;
       public bool prensIsBlockedOther=false;
-    [HideInInspector] private List<int>temp =new List<int>();
+    public List<int>temp =new List<int>();
 
     #endregion
+     Sequence sequance;
     private void Start() {
-       
+        sequance= DOTween.Sequence();
         for(int i = 0; i < quadsPlayer.Length; i++) {
             allQuands[i]=quadsPlayer[i];
             allQuands[i+1]=quadsOther[i];
@@ -103,96 +104,166 @@ public class BattleManager :MonoSingeleton<BattleManager>
         CalculateTotalPower(false);
       
 
-      for(int i = 0; i < playerStrongs.Length; i++) {
+      //for(int i = 0; i < playerStrongs.Length; i++) {
         
          if(playerStrongTotal>otherStrongTotal){// eger player otherdan yuksekse
            
-            if(otherStrongs[i]>0){
+          //  if(otherStrongs[i]>0){
+             
                 for(int j = 0; j < playerStrongs.Length; j++) {
                    for(int k = 0; k < playerStrongs.Length ; k++) {
-                    if(otherStrongs[j]>playerStrongs[k]){
+                    if(otherStrongs[j]>playerStrongs[k] && otherStrongs[j]!=0&&playerStrongs[k]!=0){
                       temp.Add(playerStrongs[k]);
-                    }
+                      Debug.Log("1");
+                   // }
                    }
                 }
                 temp.Sort();// othern herhangi bi karti bizim herhangi bi karttan yuksekse bizim dusuk kalan kartlari hangisi cope gidecek belirlemek için siraliyor
 
                 for(int m = 0; m < playerStrongs.Length; m++) {
                     if(quadsPlayer[m].GetComponentInChildren<CardDisplay>()){
+                        if(temp.Count>0)
                         if(quadsPlayer[m].GetComponentInChildren<CardDisplay>().card.strong==temp[0]){// cope gidecek karti belirliyor
                             playerArmyGarbage.AddGarbage(quadsPlayer[m].GetComponentInChildren<CardDisplay>().card);
-                            
+                            Debug.Log("2");
+                              temp.RemoveRange(0,temp.Count); //tüm tempi sıfırla;
                                 Destroy(quadsPlayer[m].GetComponentInChildren<CardDisplay>().gameObject);
-
+                                break;
                             //yine animasyonlu şekilce çöpe yollanır
                         }
                     }
 
                 }
-                
-
-                 otherArmyGarbage.AddGarbage(quadsOther[i].GetComponentInChildren<CardDisplay>().card);
+                for(int i = 0; i <otherStrongs.Length ; i++) {
+                    
+                  if(quadsOther[i].GetComponentInChildren<CardDisplay>())  {
+                    if(quadsOther[i].GetComponentInChildren<CardDisplay>().card.strong>0){
+                 
+                    otherArmyGarbage.AddGarbage(quadsOther[i].GetComponentInChildren<CardDisplay>().card);
                     // dotween aniamsyonu girer kartı yokk ederiz
                     Destroy(quadsOther[i].GetComponentInChildren<CardDisplay>().gameObject);
+                    Debug.Log("3");
+                    }
+                  
+                  }
+                 
+                }
+                    
             }
             
            
         }
         else if(playerStrongTotal==otherStrongTotal){
 
-            if(playerStrongs[i]>0){
+            for(int i = 0; i < playerStrongs.Length; i++) {
+                 if(playerStrongs[i]>0){
                 CardManager.Instance.playerArmyDeck.deck.Add(quadsPlayer[i].GetComponentInChildren<CardDisplay>().card);
-                quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.playerArmyDeck.hand.firstSpawnPos.position,.5f).OnComplete(()=> quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90, 0, 0), .1f).OnComplete(()=> Destroy(quadsPlayer[i].GetComponentInChildren<CardDisplay>().gameObject)));
+               sequance.Append(quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.playerArmyDeck.hand.firstSpawnPos.position,.5f).OnComplete(()=> quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90, 0, 0), .1f).OnComplete(()=> Destroy(quadsPlayer[i].GetComponentInChildren<CardDisplay>().gameObject)))) ;
                     // dotween aniamsyonu girer kartı yokk ederiz
                     
 
                 } 
-            if(otherStrongs[i]>0){
+            }
+           
+           for(int i = 0; i < otherStrongs.Length; i++) {
+             if(otherStrongs[i]>0){
                 
                  CardManager.Instance.otherArmyDeck.deck.Add(quadsOther[i].GetComponentInChildren<CardDisplay>().card);// her quadı army deck e eklemeye çalışıyor fixlencek
-                  quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.otherArmyDeck.hand.firstSpawnPos.position,.5f).OnComplete(()=> quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90, 0, 0), .1f).OnComplete(() => Destroy(quadsOther[i].GetComponentInChildren<CardDisplay>().gameObject)));
+                sequance.Append(quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.otherArmyDeck.hand.firstSpawnPos.position,.5f).OnComplete(()=> quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90, 0, 0), .1f).OnComplete(() => Destroy(quadsOther[i].GetComponentInChildren<CardDisplay>().gameObject))))  ;
                  // dotween aniamsyonu girer kartı yokk ederiz
             }
+           }
+           
 
 
         }
         else{
-             if(playerStrongs[i]>0){
+            // if(playerStrongs[i]>0){
                   for(int j = 0; j < playerStrongs.Length; j++) {
                    for(int k = 0; k < playerStrongs.Length ; k++) {
-                    if(playerStrongs[j]>otherStrongs[k]){
+                    if(playerStrongs[j]>otherStrongs[k]&& otherStrongs[k]!=0&&playerStrongs[j]!=0){
                       temp.Add(otherStrongs[k]);
                     }
                    }
-                }
+               // }
                 temp.Sort();
 
                 for(int m = 0; m < playerStrongs.Length; m++) {
                     if(quadsOther[m].GetComponentInChildren<CardDisplay>()){
+                        if(temp.Count>0)
                         if(quadsOther[m].GetComponentInChildren<CardDisplay>().card.strong==temp[0]){
                             otherArmyGarbage.AddGarbage(quadsOther[m].GetComponentInChildren<CardDisplay>().card);
+                            temp.RemoveRange(0,temp.Count);
                              quadsOther[m].GetComponentInChildren<CardDisplay>().transform.SetParent( otherArmyGarbage.transform);
 
                                 Destroy(quadsOther[m].GetComponentInChildren<CardDisplay>().gameObject);
                                 //yine animasyonlu şekilce çöpe yollanır
+                                break;
                             }
                     }
 
                 }
 
-
-
-                 playerArmyGarbage.AddGarbage(quadsPlayer[i].GetComponentInChildren<CardDisplay>().card);
+                for(int i = 0; i <playerStrongs.Length ; i++) {
+                    if(quadsPlayer[i].GetComponentInChildren<CardDisplay>()){
+                    playerArmyGarbage.AddGarbage(quadsPlayer[i].GetComponentInChildren<CardDisplay>().card);
                     // dotween aniamsyonu girer kartı yokk ederiz
                     Destroy(quadsPlayer[i].GetComponentInChildren<CardDisplay>().gameObject);
+                  
+                    }
+                   
+                }
+
+              
                 } 
         }
-        temp.RemoveRange(0,temp.Count); //tüm tempi sıfırla;
+      
         
-      }
+      //}
+        GoToDeck();
         WriteScore();
         resetPowers();
     }
+   
+    private void GoToDeck () {
+    for(int i = 0; i < quadsPlayer.Length; i++) {
+        if(quadsPlayer[i].GetComponentInChildren<CardDisplay>()){
+            if(quadsPlayer[i].GetComponentInChildren<CardDisplay>().card.strong>0){
+            CardManager.Instance.playerArmyDeck.deck.Add(quadsPlayer[i].GetComponentInChildren<CardDisplay>().card);
+            sequance.Append( quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.playerArmyDeck.hand.firstSpawnPos.position,.5f).SetEase(Ease.OutCubic));
+            sequance.Append( quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90,0,0),0.1f).OnComplete(()=>Destroy(quadsPlayer[i].GetComponentInChildren<CardDisplay>().gameObject)));
+            StartCoroutine(DestroyObj(quadsPlayer[i].GetComponentInChildren<CardDisplay>()));
+            }// birlik kartı desteye yolla
+            else{
+            CardManager.Instance.playerSuprotDeck.deck.Add(quadsPlayer[i].GetComponentInChildren<CardDisplay>().card);
+            sequance.Append( quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.playerSuprotDeck.hand.firstSpawnPos.position,.5f).SetEase(Ease.OutCubic));
+            sequance.Append( quadsPlayer[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90,0,0),0.1f).OnComplete(()=>Destroy(quadsPlayer[i].GetComponentInChildren<CardDisplay>().gameObject)));
+            StartCoroutine(DestroyObj(quadsPlayer[i].GetComponentInChildren<CardDisplay>()));
+            }
+           
+        }
+        
+    }
+     for(int i = 0; i < quadsOther.Length; i++) {
+        if(quadsOther[i].GetComponentInChildren<CardDisplay>()){
+            if(quadsOther[i].GetComponentInChildren<CardDisplay>().card.strong>0){
+            CardManager.Instance.otherArmyDeck.deck.Add(quadsOther[i].GetComponentInChildren<CardDisplay>().card);
+            sequance.Append( quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.otherArmyDeck.hand.firstSpawnPos.position,.5f).SetEase(Ease.OutCubic));
+            sequance.Append( quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90,0,0),0.1f));
+            StartCoroutine(DestroyObj(quadsOther[i].GetComponentInChildren<CardDisplay>()));
+            }// birlik kartı desteye yolla
+            else{
+            CardManager.Instance.otherSuportDeck.deck.Add(quadsOther[i].GetComponentInChildren<CardDisplay>().card);
+            sequance.Append( quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DOMove( CardManager.Instance.otherSuportDeck.hand.firstSpawnPos.position,.5f).SetEase(Ease.OutCubic));
+            sequance.Append( quadsOther[i].GetComponentInChildren<CardDisplay>().transform.DORotate(new Vector3(90,0,0),0.1f));
+            StartCoroutine(DestroyObj(quadsOther[i].GetComponentInChildren<CardDisplay>()));
+            }
+           
+        }
+        
+    }
+    }
+
     private void resetPowers()
     {
         for (int i = 0; i < playerStrongs.Length; i++)
@@ -265,6 +336,11 @@ public void ChoseSkill () {
     public void RefreshGrapichs () {
         
     }
-
+IEnumerator DestroyObj(CardDisplay obj){
+    yield return new WaitForSeconds(.6f);
+       if(obj)
+       Destroy(obj.gameObject);
+   
+  }
 
 }
